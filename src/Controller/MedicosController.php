7eefc1b5330e8e431b0a8150ed1c75c1e60de132
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2020-11-16 19:35:58 -0300

    Aula 03

--- src/Controller/MedicosController.php
+++ src/Controller/MedicosController.php
@@ -3,15 +3,26 @@
 namespace App\Controller;
 
 use App\Entity\Medico;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
-class MedicosController
+class MedicosController extends AbstractController
 {
     /**
-     * @Route("/medicos/", methods={"POST"})
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/medicos", methods={"POST"})
      * @param Request $request
      * @return Response
      */
@@ -24,6 +35,39 @@ class MedicosController
         $medico->crm = $dadoEmJson->crm;
         $medico->nome = $dadoEmJson->nome;
 
+        $this->entityManager->persist($medico);
+        $this->entityManager->flush();
+
         return new JsonResponse($medico);
     }
+
+    /**
+     * @Route("/medicos", methods={"GET"})
+     * @return Response
+     */
+    public function buscarTodos(): Response
+    {
+        $medicoRepository = $this->getDoctrine()->getRepository(Medico::class);
+        $medicos = $medicoRepository->findAll();
+
+        return new JsonResponse($medicos);
+    }
+
+    /**
+     * @Route("/medicos/{id}", methods={"GET"})
+     * @param Request $request
+     * @return Response
+     */
+    public function buscarUm(Request $request): Response
+    {
+        $id = $request->get('id');
+
+        $medicoRepository = $this->getDoctrine()->getRepository(Medico::class);
+        $medico = $medicoRepository->find($id);
+
+        $codigoRetorno = is_null($medico) ? Response::HTTP_NO_CONTENT : 200;
+
+        return new JsonResponse($medico, $codigoRetorno);
+    }
+
 }
--- src/Entity/Medico.php
+++ src/Entity/Medico.php
@@ -2,8 +2,23 @@
 
 namespace App\Entity;
 
+/**
+ * @ORM\Entity()
+ */
 class Medico
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    public int $id;
+    /**
+     * @ORM\Column(type="integer")
+     */
     public int $crm;
+    /**
+     * @ORM\Column(type="string")
+     */
     public string $nome;
 }
